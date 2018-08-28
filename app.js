7d1fb18f269870aadf4f9e6dacec3a22c894972e
Sarah <yamashige.sarah@gmail.com>
2018-08-27 17:51:24 -1000

    made sure express was installed

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const qs = require('querystring');
+const bp = require('body-parser');
+
+
+
+// **** Test **** //
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// const server = app.listen(3000, () => {
+//     const host = server.address().address;
+//     const port = server.address().port;
+
+//     console.log(`Example app listening at http://${host}:${port}`);
+// });
+
